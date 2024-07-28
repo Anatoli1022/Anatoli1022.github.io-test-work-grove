99a6bedeb0903acfd5c3ada00d8bffbde80bc37f
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-29 02:23:52 +0300

    feat: adds new logic for table and row

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -3,12 +3,17 @@ import Cell from './Cell';
 
 interface RowProps {
   row: { title: string; cells: boolean[] };
+  onEdit: () => void;
+  onDelete: () => void;
 }
 
-const Row: React.FC<RowProps> = ({ row }) => (
+const Row: React.FC<RowProps> = ({ row, onDelete, onEdit }) => (
   <tr>
+    <td>
+      <button onClick={onEdit}>Редактировать</button>
+      <button onClick={onDelete}>Удалить</button>
+    </td>
     <td>{row.title}</td>
-
     {row.cells.map((cell, cellIndex) => (
       <Cell key={cellIndex} index={cellIndex} value={cell} />
     ))}
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { generateColumns, generateRows } from '../utils/table';
+import {
+  generateColumns,
+  generateRows,
+  addRow,
+  deleteRow,
+  editRow,
+} from '../utils/table';
 import Row from './Row';
+import Modal from './Modal';
 
 const Table: React.FC = () => {
   const [columns, setColumns] = useState<string[]>([]);
   const [rows, setRows] = useState<{ title: string; cells: boolean[] }[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [rowIndex, setRowIndex] = useState<number | null>(null);
+  const [operation, setOperation] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,22 +27,79 @@ const Table: React.FC = () => {
     fetchData();
   }, []);
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          {columns &&
-            columns.map((colum, index) => <th key={index}>{colum}</th>)}
-        </tr>
-      </thead>
+  const handleOperationRow = ({
+    index,
+    operation,
+  }: {
+    index: number | null;
+    operation: string;
+  }) => {
+    setRowIndex(index);
+    setOperation(operation);
+    setIsModalOpen(true);
+  };
 
-      <tbody>
-        {rows.map((row, index) => (
-          <Row key={index} row={row} />
-        ))}
-      </tbody>
-    </table>
+  const confirm = (operation: string | null) => {
+    switch (operation) {
+      case 'deleteRow':
+        if (rowIndex !== null) {
+          deleteRow(rowIndex, rows, setRows);
+        }
+        break;
+      case 'editRow':
+        if (rowIndex !== null) {
+          editRow(rowIndex, rows, setRows);
+        }
+        break;
+      case 'addRow':
+        addRow({ rows, setRows, columns });
+        break;
+      default:
+        break;
+    }
+    setOperation(null);
+    setIsModalOpen(false);
+  };
+
+  const cancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            {columns &&
+              columns.map((colum, index) => <th key={index}>{colum}</th>)}
+          </tr>
+        </thead>
+
+        <tbody>
+          {rows.map((row, index) => (
+            <Row
+              key={index}
+              row={row}
+              onDelete={() =>
+                handleOperationRow({ index, operation: 'deleteRow' })
+              }
+              onEdit={() => handleOperationRow({ index, operation: 'editRow' })}
+            />
+          ))}
+        </tbody>
+      </table>
+      <button
+        onClick={() => handleOperationRow({ index: null, operation: 'addRow' })}
+      >
+        Добавить строку
+      </button>
+
+      {isModalOpen && (
+        <Modal operation={operation} confirm={confirm} cancel={cancel} />
+      )}
+    </div>
   );
 };
 
