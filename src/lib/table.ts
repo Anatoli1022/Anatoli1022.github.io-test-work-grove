9d8a3be297f7c2f842fa831a86fb468b4bee46dd
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-29 16:37:04 +0300

    fix: fix row , colums count , timeout

--- src/lib/table.ts
+++ src/lib/table.ts
@@ -8,26 +8,26 @@ interface Row {
 export async function generateColumns(): Promise<string[]> {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const numColumns = getRandomInt(2, 5);
+      const numColumns = getRandomInt(2, 100);
       const columns = Array.from(
         { length: numColumns },
         (_, index) => `Обработка ${index + 1}`
       );
       resolve(columns);
-    }, 40);
+    }, 1500);
   });
 }
 
 export async function generateRows(numColumns: number): Promise<Row[]> {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const numRows = getRandomInt(2, 5);
+      const numRows = getRandomInt(2, 100);
       const rows = Array.from({ length: numRows }, (_, index) => ({
         title: `Заказ ${index + 1}`,
         cells: Array.from({ length: numColumns }, () => Math.random() < 0.5),
       }));
       resolve(rows);
-    }, 40);
+    }, 1500);
   });
 }
 
